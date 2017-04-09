90ced63c3a312f1a1edddc9cf2b1ea2f92540d91
FireAwayH <hejiheji001@gmail.com>
2017-04-09 23:14:11 +0800

    A GreaseMonkey Like Node.JS Platform
    Init Ver

--- app.js
+++ app.js
@@ -49,9 +49,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-});
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// });
 
 (function () {
     //create cert when you want to use https features
